22091c579376c40254a3c39e723b49b7c352c073
Armon Khosravi <armonk@gmail.com>
2022-10-02 20:14:35 -0400

    Fix indentation on all bash scripts (2 spaces)

--- macos/install-applications.sh
+++ macos/install-applications.sh
@@ -7,15 +7,15 @@ _install() {
 }
 
 install_applications() {
-    _install android-studio
-    _install flipper
-    _install flux
-    _install google-chrome
-    # Tool for OS X automation: https://www.hammerspoon.org/
-    _install hammerspoon
-    # Open source bettersnaptool alternative
-    _install rectangle
-    _install slack
-    _install spotify
-    _install visual-studio-code
+  _install android-studio
+  _install flipper
+  _install flux
+  _install google-chrome
+  # Tool for OS X automation: https://www.hammerspoon.org/
+  _install hammerspoon
+  # Open source bettersnaptool alternative
+  _install rectangle
+  _install slack
+  _install spotify
+  _install visual-studio-code
 }
--- vscode/setup.sh
+++ vscode/setup.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
 install_vscode_extensions() {
-    # Visual Studio Code :: Extension list
-    local -a pkglist=(
-        DavidAnson.vscode-markdownlint
-        dbaeumer.vscode-eslint
-        donjayamanne.githistory
-        eamodio.gitlens
-        esbenp.prettier-vscode
-        golang.go
-        Gruntfuggly.todo-tree
-        k--kato.intellij-idea-keybindings
-        mohsen1.prettify-json
-        ms-vscode.cpptools
-        ms-vscode.vscode-typescript-tslint-plugin
-        naumovs.color-highlight
-        rbbit.typescript-hero
-        streetsidesoftware.code-spell-checker
-        timonwong.shellcheck
-        Tyriar.lorem-ipsum
-        # Ruby extensions
-        rebornix.ruby
-        wingrunr21.vscode-ruby
-        castwide.solargraph
-    )
+  # Visual Studio Code :: Extension list
+  local -a pkglist=(
+    DavidAnson.vscode-markdownlint
+    dbaeumer.vscode-eslint
+    donjayamanne.githistory
+    eamodio.gitlens
+    esbenp.prettier-vscode
+    golang.go
+    Gruntfuggly.todo-tree
+    k--kato.intellij-idea-keybindings
+    mohsen1.prettify-json
+    ms-vscode.cpptools
+    ms-vscode.vscode-typescript-tslint-plugin
+    naumovs.color-highlight
+    rbbit.typescript-hero
+    streetsidesoftware.code-spell-checker
+    timonwong.shellcheck
+    Tyriar.lorem-ipsum
+    # Ruby extensions
+    rebornix.ruby
+    wingrunr21.vscode-ruby
+    castwide.solargraph
+  )
 
-    for EXT in "${pkglist[@]}"; do
-        code --install-extension "$EXT"
-    done
+  for EXT in "${pkglist[@]}"; do
+    code --install-extension "$EXT"
+  done
 }
 
 # Create symbolic link for settings.json
 # param - location of  dotfiles directory
 symlink_vscode_settings() {
-    # Note: when creating symbolic links we need to use the absolute source path or won't work
-    ln -fs "${1}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+  # Note: when creating symbolic links we need to use the absolute source path or won't work
+  ln -fs "${1}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
 }
